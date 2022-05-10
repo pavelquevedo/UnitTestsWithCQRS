ee9744b79ae671c8a114079b0416436a1a1db4e4
Pavel Quevedo <pavel.quevedo@rivetlogic.com>
2022-05-10 16:48:06 -0600

    Unit tests for strings, setting up an instance for all the test methods.

---
+++ MyLib/Client.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLib
+{
+    public class Client
+    {
+        public string ClientName { get; set; }
+        public string CreateCompleteName(string firstName, string lastName)
+        {
+            ClientName = $"{firstName} {lastName}";
+            return ClientName;
+        }
+    }
+}
---
+++ MyLibNUnit.Test/ClientNUnitTest.cs
@@ -0,0 +1,67 @@
+﻿using MyLib;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLibNUnit.Test
+{
+    [TestFixture]
+    public class ClientNUnitTest
+    {
+        private Client client; 
+
+        [SetUp]
+        public void Setup()
+        {
+            client = new();
+        }
+
+        [Test]
+        public void CreateCompleteName_InputFirstNameLastName_ReturnsCompleteName()
+        {
+            //Arrange
+            //Client client = new();
+
+            //Act 
+            string completeName = client.CreateCompleteName("Pavel", "Quevedo");
+
+            //Assert
+            Assert.That(completeName, Is.EqualTo("Pavel Quevedo"));
+            Assert.AreEqual(completeName, "Pavel Quevedo");
+            Assert.That(completeName, Does.Contain("Quevedo"));
+            Assert.That(completeName, Does.Contain("quevedo").IgnoreCase);
+            Assert.That(completeName, Does.StartWith("Pavel"));
+            Assert.That(completeName, Does.EndWith("Quevedo"));
+        }
+
+        [Test]
+        public void CreateCompleteName_NoValues_ReturnsNull()
+        {
+            //Arrange
+            //Client client = new();
+
+            //Act 
+
+
+            //Assert
+            Assert.IsNull(client.ClientName);
+        }
+
+        [Test]
+        [TestCase("Pavel","Quevedo")]
+        public void CreateCompleteName_InputFirstNameLastName_ReturnsNotNull(string firstName, string lastName)
+        {
+            //Arrange
+            //Client client = new();
+
+            //Act 
+            client.CreateCompleteName(firstName, lastName);
+
+            //Assert
+            Assert.IsNotNull(client.ClientName);
+        }
+    }
+}
