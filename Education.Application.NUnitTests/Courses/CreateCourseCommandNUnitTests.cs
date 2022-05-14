bc774901a1570948cdee27744f0dd82e9a95a9cc
Pavel Quevedo <pavel.quevedo@rivetlogic.com>
2022-05-13 18:10:33 -0600

    Adding unit tests for CreateCourseCommand and GetCourseByIdQuery.

---
+++ Education.Application.NUnitTests/Courses/CreateCourseCommandNUnitTests.cs
@@ -0,0 +1,67 @@
+﻿using AutoFixture;
+using AutoMapper;
+using Education.Application.Courses;
+using Education.Application.NUnitTests.Helpers;
+using Education.Domain;
+using Education.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Education.Application.NUnitTests.Courses
+{
+    [TestFixture]
+    public class CreateCourseQueryNUnitTests
+    {
+        private CreateCourseCommand.CreateCourseCommandHandler handlerCreateCourse;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var fixture = new Fixture();
+            var courseRecords = fixture.CreateMany<Course>().ToList();
+
+            courseRecords.Add(fixture.Build<Course>()
+                .With(tr => tr.CourseId, Guid.Empty)
+                .Create());
+
+            //Creating temporary in-memory database
+            var options = new DbContextOptionsBuilder<EducationDbContext>()
+                .UseInMemoryDatabase(databaseName: $"EducationDbContext-{Guid.NewGuid()}")
+                .Options;
+
+            //Creating fake dbcontext
+            var educationDbContextFake = new EducationDbContext(options);
+            educationDbContextFake.Courses.AddRange(courseRecords);
+            educationDbContextFake.SaveChanges();
+
+            handlerCreateCourse = new CreateCourseCommand.CreateCourseCommandHandler(educationDbContextFake);
+        }
+
+        [Test]
+        public async Task CreateCourseCommand_InputCourse_ReturnsNumber()
+        {
+            //Arrange           
+            CreateCourseCommand.CreateCourseCommandRequest request = new()
+            {
+                Name = "Course test",
+                Description = "Description test",
+                PublishDate = DateTime.Now.AddYears(2),
+                Price = 100
+            };
+            //and pass it the objects context and mapping as parameters
+
+            //Act
+            var result = await handlerCreateCourse.Handle(request, new CancellationToken());
+
+            //Assert
+            Assert.That(result, Is.EqualTo(Unit.Value));
+        }
+    }
+}
---
+++ Education.Application.NUnitTests/Courses/GetCourseByIdQueryNUnitTests.cs
@@ -0,0 +1,73 @@
+﻿using AutoFixture;
+using AutoMapper;
+using Education.Application.Courses;
+using Education.Application.NUnitTests.Helpers;
+using Education.Domain;
+using Education.Persistence;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Education.Application.NUnitTests.Courses
+{
+    [TestFixture]
+    public class GetCourseByIdQueryNUnitTests
+    {
+        private GetCourseByIdQuery.GetCourseByIdQueryHandler handlerByIdCourse;
+        private Guid courseIdTest;
+
+        [SetUp]
+        public void SetUp()
+        {
+            courseIdTest = new Guid("3de6334b-9aaf-46dc-9231-561267dd8289");
+
+            var fixture = new Fixture();
+            var courseRecords = fixture.CreateMany<Course>().ToList();
+
+            courseRecords.Add(fixture.Build<Course>()
+                .With(tr => tr.CourseId, courseIdTest)
+                .Create());
+
+            //Creating temporary in-memory database
+            var options = new DbContextOptionsBuilder<EducationDbContext>()
+                .UseInMemoryDatabase(databaseName: $"EducationDbContext-{Guid.NewGuid()}")
+                .Options;
+
+            //Creating fake dbcontext
+            var educationDbContextFake = new EducationDbContext(options);
+            educationDbContextFake.Courses.AddRange(courseRecords);
+            educationDbContextFake.SaveChanges();
+
+            //Configuring fake mapper
+            var mapConfig = new MapperConfiguration(cfg => {
+                cfg.AddProfile(new MappingTest());
+            });
+
+            var mapper = mapConfig.CreateMapper();
+
+            handlerByIdCourse = new GetCourseByIdQuery.GetCourseByIdQueryHandler(educationDbContextFake, mapper);
+        }
+
+        [Test]
+        public async Task GetCourseByIdQueryHandler_InputCourseId_ReturnsNotNull()
+        {
+            //Arrange
+            GetCourseByIdQuery.GetCourseByIdQueryRequest request = new()
+            {
+                id = courseIdTest
+            };
+            //and pass it the objects context and mapping as parameters
+
+            //Act
+            var result = await handlerByIdCourse.Handle(request, new CancellationToken());
+
+            //Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
